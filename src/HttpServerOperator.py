5ff3e75af7255fa02dbad6809401c5c9250062f1
ccxAlpha <oskar.bukowski1@gmail.com>
2022-04-11 07:02:24 +0200

    added new API functionalities

--- src/HttpServerOperator.py
+++ src/HttpServerOperator.py
@@ -21,5 +21,50 @@ def get_exchanges():
     db = DatabaseExecutor(query)
     return jsonify(exchanges=db.exchanges())
 
+@app.route('/markets', methods=['GET'])
+def get_markets():
+    query = '''SELECT table_name, table_schema 
+    FROM   information_schema.tables
+    where not table_schema = 'information_schema'
+    and NOT (table_name ~* '^pg' or table_name ~* '^sql')
+    and table_name like '%%_ob'; '''
+
+    db = DatabaseExecutor(query)
+    return jsonify(markets=db.markets())
+
+@app.route('/trades&ex=<string:exchange>&market=<string:market>&limit=<int:limit>', methods=['GET'])
+def get_trades(exchange, market, limit):
+    query = '''SELECT * FROM {}.{}_trades
+    ORDER BY timestamp DESC
+    LIMIT {}'''.format(exchange, market, limit)
+
+    db = DatabaseExecutor(query)
+    return jsonify(db.trades())
+
+
+@app.route('/orderbook&ex=<string:exchange>&market=<string:market>&limit=<int:limit>', methods=['GET'])
+def get_orderbooks(exchange, market, limit):
+    query = '''SELECT * FROM {}.{}_ob
+    ORDER BY timestamp DESC
+    LIMIT {}'''.format(exchange, market, limit)
+
+    db = DatabaseExecutor(query)
+    return jsonify(db.orderbook())
+
+@app.route('/orderbook_range&ex=<string:exchange>&market=<string:market>&ts_range=<int:ts_from>,<int:ts_to>',
+           methods=['GET'])
+def get_orderbooks_range(exchange, market, ts_from, ts_to):
+    query = '''select * from {}.{}_ob
+    where "timestamp" between {} and {}'''.format(exchange, market, ts_from, ts_to)
+
+    db = DatabaseExecutor(query)
+    return jsonify(db.orderbook())
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, ssl_context='adhoc')
--- src/run.sh
+++ src/run.sh
@@ -2,4 +2,6 @@
 
 export FLASK_APP=/home/api/HttpServerOperator.py
 
+# export FLASK_APP=HttpServerOperator.py
+
 flask run -h 0.0.0.0
